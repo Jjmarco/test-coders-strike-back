91c1376bdb35305937d6bae09d70e12f6bf4f30e
Jean-Marc Abbas <jjmarco75@yahoo.fr>
2019-01-25 14:56:23 +0100

    Passage à la ligue Or
    Ajout action SHIELD
    Ajout compensation de dérapage pour des virages plus serrés
    Optimisations

--- code.cpp
+++ code.cpp
@@ -11,7 +11,7 @@ public:
     double x, y;
     Vector(double x, double y) : x(x), y(y) {}
     Vector() : Vector(0., 0.) {}
-    Vector(Vector &o) : Vector(o.x, o.y) {}
+    Vector(const Vector& o) : Vector(o.x, o.y) {}
     
     void set(double _x, double _y) {
         x = _x;
@@ -23,10 +23,11 @@ public:
         y = _y;
     }
     
-    void set(Vector &o) {
+    void set(const Vector& o) {
         set(o.x, o.y);
     }
-    double norm() {
+    
+    double norm() const {
         return sqrt(x*x + y*y);
     }
     
@@ -36,16 +37,47 @@ public:
         y /= n;
     }
     
-    double dot(Vector &o) {
+    Vector getNormalized() const {
+        Vector v(*this);
+        v.normalize();
+        return v;
+    }
+    
+    void add(Vector& o) {
+        x += o.x;
+        y += o.y;
+    }
+    
+    void scalar(double s) {
+        x *= s;
+        y *= s;
+    }
+    
+    double dot(const Vector& o) const {
         return x*o.x + y*o.y;
     }
 };
+
+Vector operator+(const Vector& u, const Vector& v) {
+    return Vector(u.x + v.x, u.y + v.y);
+}
+
+Vector operator*(double s, const Vector& u) {
+    return Vector(s*u.x, s*u.y);
+}
+
+Vector operator-(const Vector& u, const Vector& v) {
+    return operator+(u, -1*v);
+}
+
+
 
 int main()
 {
     constexpr int maxThrust = 100;
     constexpr int boostDistThresh = 5000;
     constexpr int boostAngleTolerance = 7;
+    constexpr double slowdownMult = .06;
     
     int x, y;
     int nextCheckpointX, nextCheckpointY;
@@ -56,7 +88,8 @@ int main()
     int newThrust = 0;
     bool boost = false, boostInUse = false, boostUsed = false;
     
-    Vector podToChkp, podToOpp;
+    Vector podToChkp, podToOpp, moveDelta, newTarget;
+    int lastX = 0, lastY = 0;
     
     while (true) {
         /* --- Entrée --- */
@@ -67,33 +100,52 @@ int main()
             >> opponentX >> opponentY;
         cin.ignore();
         
+        podToChkp.set(nextCheckpointX - x, nextCheckpointY - y);
+        podToOpp.set(opponentX - x, opponentY - y);
+        moveDelta.set(x - lastX, y - lastY);
+        
         /* --- Calcule des nouveaux paramètres --- */
         
         // Calcule la poussée en fonction de l'angle entre le sens du mouvement et le checkpoint
-        newThrust = min(maxThrust, max(0, (int)(.09*nextCheckpointDist*cos(M_PI*nextCheckpointAngle/180))));
+        newThrust = min(maxThrust, max(0, (int)(slowdownMult*nextCheckpointDist*cos(M_PI*nextCheckpointAngle/180))));
         
-        podToChkp.set(nextCheckpointX - x, nextCheckpointY - y);
-        podToOpp.set(opponentX - x, opponentY - y);
-        if(podToOpp.norm() < 1100) {
-            podToChkp.normalize();
-            podToOpp.normalize();
+        string thrustCmd;
+        if(podToOpp.norm() < 1000) {
             // Calcule le cosinus de l'angle entre le vecteur Pod->Checkpoint et le vecteur Pod->Adversaire
             // Si l'ennemi est dans une zone proche à l'arrière du pod, le pod ralenti afin de tenter de le ralentir
-            if(podToChkp.dot(podToOpp) < 0)
-                newThrust *= .3;
+            if(podToChkp.getNormalized().dot(podToOpp.getNormalized()) < -.7)
+            {
+                if(nextCheckpointDist > 1600)
+                {
+                    newThrust *= .3;
+                }
+                else
+                {
+                    thrustCmd = "SHIELD";
+                }
+            }
+        }
+        
+        // Compensation de dérapage
+        int targetX = nextCheckpointX,
+            targetY = nextCheckpointY;
+        if(lastX != 0 && lastY != 0)
+        {  
+            newTarget = podToChkp - 1.5*moveDelta;
+            targetX = newTarget.x + x;
+            targetY = newTarget.y + y;
         }
         
         // active le boost sur une ligne droite et si le checkpoint est assez loin pour éviter les dérapages
         boost = nextCheckpointDist > boostDistThresh
               && abs(nextCheckpointAngle) < boostAngleTolerance;
               
-        string thrustCmd;
         if(boost && !boostUsed)
         {
             thrustCmd = "BOOST";
             boostInUse = true;
         }
-        else
+        else if(thrustCmd != "SHIELD")
         {
             thrustCmd = to_string(newThrust);
             if(boostInUse) boostUsed = true;
@@ -102,6 +154,8 @@ int main()
         
         /* --- Sortie --- */
         
-        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrustCmd << endl;
+        cout << targetX << " " << targetY << " " << thrustCmd << endl;
+        lastX = x;
+        lastY = y;
     }
 }
